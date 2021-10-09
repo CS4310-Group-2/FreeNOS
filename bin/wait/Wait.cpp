c9249c9a439391b52312b25e1f982418c1456944
Jeffrey McDonald <jffrmcdonald@gmail.com>
2021-10-08 22:18:46 -0700

    modified argument for wait to be handled by posix instead

--- bin/wait/Wait.cpp
+++ bin/wait/Wait.cpp
@@ -5,15 +5,13 @@
 #include <unistd.h>
 #include "Wait.h"
 
-int pid;
+
 
 Wait::Wait(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
     parser().setDescription("Cause a process to wait for some time");
     parser().registerPositional("PID", "Process waits for certain Process given by PID");
-    
-    pid = atoi(argv[1]);
 }
 
 Wait::~Wait()
@@ -24,7 +22,7 @@ Wait::~Wait()
 
 Wait::Result Wait::exec()
 {
-    int myPID = pid;
+    int myPID = atoi(arguments().get("PID"));
     int status;
     printf("Waiting on PID %d\n",myPID);
     pid_t wait_call_status = waitpid(myPID,&status,0);
