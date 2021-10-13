84e65ba77f2f99f144f9769aef6cf7dda5c32516
Jeffrey McDonald <jffrmcdonald@gmail.com>
2021-10-12 17:18:00 -0700

    properly showed the info status in ps-l

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -62,7 +62,7 @@ ProcessList::Result ProcessList::exec()
                 char line[128];
                 snprintf(line, sizeof(line),
                         "%3d %7d %8d %4d %5d %10s %32s\r\n",
-                        pid, info.kernelState.parent,info.PriorityLevel,
+                        pid, info.kernelState.parent,info.kernelState.prioritylevel,
                         0, 0, *info.textState, *info.command);
                 out << line;
             }
