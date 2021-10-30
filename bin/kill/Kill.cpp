53a5c57cd67b8f18e78e08b52a3eb8bfa60e4266
Tyler Yamaguchi <tmyamaguchi@cpp.edu>
2021-10-30 15:47:27 -0700

    Added a self-check to avoid kill being used on itself

--- bin/kill/Kill.cpp
+++ bin/kill/Kill.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include "Kill.h"
 #include "signal.h"
+#include <ProcessClient.h>
 
 Kill::Kill(int argc, char **argv): POSIXApplication(argc,argv)
 {
@@ -22,14 +23,37 @@ Kill::~Kill()
 Kill::Result Kill::exec()
 {
     int pid = atoi(arguments().get("PID"));
+        ProcessClient::Info info;
+        const ProcessClient process;
+        const ProcessClient::Result result = process.processInfo(pid, info);
+        if(result != ProcessClient::Success)
+        {
+          ERROR("Process " << pid << " was not found.")
+          return InvalidArgument;
+        }
+        if (info.textState != "Running")
+        {
+
+          int wasKilled = kill(pid,SIGKILL);
+          if(wasKilled == 0){//Kill returned 0, success.
+          printf("Process %d was successfully killed.\n", pid);
+          return Success;
+          }else{//Kill returned -1, failure.
+          ERROR("Process " << pid << " was not killed.");
+          return InvalidArgument;
+          }
+        }else{
+
+            ERROR("Kill cannot be used on itself.");
+            return InvalidArgument;
+        }
+        /*
     int result = kill(pid,SIGKILL);
-    if(result == 0)
-    {//Kill returned 0, success.
+    if(result == 0){//Kill returned 0, success.
        printf("Process %d was successfully killed.\n", pid);
        return Success;
-    }else
-    {//Kill returned -1, failure.
+    }else{//Kill returned -1, failure.
       ERROR("Process " << pid << " was not killed.");
-      return IOError;
-    }
+      return InvalidArgument;
+    }*/
 }
